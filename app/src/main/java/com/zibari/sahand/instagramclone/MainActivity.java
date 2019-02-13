43971f9b0884366a188faff64c914f3a17d6c2ec
smz1989 <sahandmohammadiziabari@gmail.com>
2019-02-13 19:20:51 +0100

    SignUp and Login Parse

--- app/src/main/java/com/zibari/sahand/instagramclone/MainActivity.java
+++ app/src/main/java/com/zibari/sahand/instagramclone/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zibari.sahand.instagramclone;
 
 import android.app.DownloadManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -120,6 +121,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(MainActivity.this, SignUpLoginActivity.class);
+                startActivity(intent);
 
 
             }
